8378c9a61d896fc073281e26b0268a1afa858d92
Jackson Nolan <jnolan5@mgh.harvard.edu>
2024-12-11 15:29:33 -0500

    Revert "enable build testing with native Qt libs"
    This reverts commit 64b9fbd4a8f1ee64a2f817b4e18223d2ac2cebf2.

--- cmake/FindQT.cmake
+++ cmake/FindQT.cmake
@@ -42,75 +42,22 @@ elseif(Qt5_DIR)
    endif()
 endif()
 
-function(install_qt5_lib_links)
-   install(CODE "
-     message(STATUS \"Install links to native qt5 libs\")
-     file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib/qt/lib)
-     message(STATUS \"(cd ${Qt5_BASE}/lib && tar cpf - lib*) | (cd ${CMAKE_INSTALL_PREFIX}/lib/qt/lib && tar xvpf - )\")
-     execute_process(COMMAND bash -c \"(cd ${Qt5_BASE}/lib && tar cpf - lib*) | (cd ${CMAKE_INSTALL_PREFIX}/lib/qt/lib && tar xvpf - )\" RESULT_VARIABLE retcode)
-     if(NOT \${retcode} STREQUAL 0)
-       message(FATAL_ERROR \"Could not install qt5 lib links\")
-     endif()"
-     COMPONENT qtlib
-   )
-endfunction()
-
-function(install_qt5_plugins_platform_links)
-   install(CODE "
-     message(STATUS \"Install links to native qt5 platform plugins\")
-     file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib/qt/plugins)
-     message(STATUS \"(cd ${Qt5_BASE}/plugins && tar cpf - platforms) | (cd ${CMAKE_INSTALL_PREFIX}/lib/qt/plugins && tar xvpf - )\")
-     execute_process(COMMAND bash -c \"(cd ${Qt5_BASE}/plugins && tar cpf - platforms) | (cd ${CMAKE_INSTALL_PREFIX}/lib/qt/plugins && tar xvpf - )\" RESULT_VARIABLE retcode)
-     if(NOT \${retcode} STREQUAL 0)
-       message(FATAL_ERROR \"Could not install qt5 platform plugins\")
-     endif()"
-     COMPONENT qtplatform
-   )
-endfunction()
-
-function(install_qt5_plugins_imageformat_links)
-   install(CODE "
-     message(STATUS \"Install links to native qt5 imageformat plugins\")
-     file(MAKE_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib/qt/plugins)
-     message(STATUS \"(cd ${Qt5_BASE}/plugins && tar cpf - imageformats) | (cd ${CMAKE_INSTALL_PREFIX}/lib/qt/plugins && tar xvpf - )\")
-     execute_process(COMMAND bash -c \"(cd ${Qt5_BASE}/plugins && tar cpf - imageformats) | (cd ${CMAKE_INSTALL_PREFIX}/lib/qt/plugins && tar xvpf - )\" RESULT_VARIABLE retcode)
-     if(NOT \${retcode} STREQUAL 0)
-       message(FATAL_ERROR \"Could not install qt5 imageformat plugins\")
-     endif()"
-     COMPONENT qtimage
-   )
-endfunction()
-
 # install the shared libraries to the freesurfer lib directory
 if(Qt5_FOUND AND NOT APPLE)
-  if (NOT QT_SYSLIBS)
-     file(GLOB _qt_libs_to_install "${Qt5_INSTALL_DIR}/lib/lib*.so*")
-     if(_qt_libs_to_install)
-       install(PROGRAMS ${_qt_libs_to_install} DESTINATION lib/qt/lib)
-       # add Qt library directory to rpath
-       set(CMAKE_INSTALL_RPATH "$ORIGIN/../lib/qt/lib:${CMAKE_INSTALL_RPATH}")
-       # install the platform plugins
-       if(EXISTS ${Qt5_INSTALL_DIR}/plugins/platforms)
-         install(DIRECTORY ${Qt5_INSTALL_DIR}/plugins/platforms DESTINATION lib/qt/plugins)
-       endif()
-       # install the image format plugins
-       if(EXISTS ${Qt5_INSTALL_DIR}/plugins/imageformats)
-         install(DIRECTORY ${Qt5_INSTALL_DIR}/plugins/imageformats DESTINATION lib/qt/plugins)
-       endif()
-       # make sure executables know where to find the plugins directory
-       install(FILES ${CMAKE_SOURCE_DIR}/qt/qt.conf DESTINATION bin)
-     endif()
-  else()
-     # The /usr/pubsw/packages qt distro contains soft links pointing to the system Qt5 files,
-     # so install via functions and do not use EXISTS/GLOB tests.
-     install_qt5_lib_links()
-     # add Qt library directory to rpath
-     set(CMAKE_INSTALL_RPATH "$ORIGIN/../lib/qt/lib:${CMAKE_INSTALL_RPATH}")
-     # install the platform plugins
-     install_qt5_plugins_platform_links()
-     # install the image format plugins
-     install_qt5_plugins_imageformat_links()
-     # make sure executables know where to find the plugins directory
-     install(FILES ${CMAKE_SOURCE_DIR}/qt/qt.conf DESTINATION bin)
+  file(GLOB _qt_libs_to_install "${Qt5_INSTALL_DIR}/lib/lib*.so*")
+  if(_qt_libs_to_install)
+    install(PROGRAMS ${_qt_libs_to_install} DESTINATION lib/qt/lib)
+    # add Qt library directory to rpath
+    set(CMAKE_INSTALL_RPATH "$ORIGIN/../lib/qt/lib:${CMAKE_INSTALL_RPATH}")
+    # install the platform plugins
+    if(EXISTS ${Qt5_INSTALL_DIR}/plugins/platforms)
+      install(DIRECTORY ${Qt5_INSTALL_DIR}/plugins/platforms DESTINATION lib/qt/plugins)
+    endif()
+    # install the image format plugins
+    if(EXISTS ${Qt5_INSTALL_DIR}/plugins/imageformats)
+      install(DIRECTORY ${Qt5_INSTALL_DIR}/plugins/imageformats DESTINATION lib/qt/plugins)
+    endif()
+    # make sure executables know where to find the plugins directory
+    install(FILES ${CMAKE_SOURCE_DIR}/qt/qt.conf DESTINATION bin)
   endif()
 endif()
